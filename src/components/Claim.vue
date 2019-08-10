6633d516409fd60cf320df4a5df1167b7e1044c9
Vezil <SzymonWojaczek@wp.pl>
2019-08-10 17:12:27 +0200

    Add Basic hero image view

---
+++ src/components/Claim.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="claimWrapper">
+    <h1 class="claim">Spacer</h1>
+    <p class="subclaim">Begin your journey through our amazin galaxy, and discover places you never even heard of.</p>
+    <p class="subclaim">Type anything space-related to start.</p>
+  </div>
+</template>
+<script>
+export default {
+ name:'Claim',
+};
+</script>
+<style lang="scss" scoped>
+  .claimWrapper {
+    display:flex;
+    flex-direction: column;
+    text-align:center;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .claim {
+    font-size: 50px;
+    margin:0;
+  }
+
+   .subclaim {
+    margin: 20px 0 0 0;
+    font-size: 13px;
+  }
+
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-        <input id="search" name="search" v-model="searchValue" @input="handleInput"/>
+    <Claim/>
+    <SearchInput/>
+    
+      
 
-        <ul>
+      <!--  <ul>
           <li v-for="item in results" :key="item.data[0].nasa_id">
             <p>{{ item.data[0].description }}</p>
           </li>
         </ul>
-    </div>
+        -->
+    
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
     name:'Search',
+    components:{
+      Claim,
+      SearchInput,
+    },
     data(){
       return{
           searchValue: '',
@@ -49,29 +57,16 @@ export default {
      display:flex;
      flex-direction: column;
      align-items:center;
+     justify-content: center;
      width:100%;
+     height:100vh;
      padding:30px;
      margin:0;
+     background-image: url('../assets/heroimage.jpg');
+     background-repeat:no-repeat;
+     background-size:cover;
+     background-position:80% 0%;
   }
-  .search{
-    display:flex;
-    flex-direction: column;
-    width: 250px;
-  }
-
-  label{
-
-    font-family: Montserrat, sans-serif;
-  }
-
-
-  input{
-    height: 30px;
-    border: 0;
-    border-bottom: 1px solid black;
-    
-  }
-
-
+  
 </style>
 
